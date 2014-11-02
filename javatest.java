1427208a727398e238826181fe278f4eb20faf2a
jeje1610 <jeje1610@daum.net>
2014-11-03 00:44:45 +0900

    Add javatest.java

---
+++ javatest.java
@@ -0,0 +1,17 @@
+class testGit{
+
+	int a=10;
+	int b=20;
+
+	public int plus(int a, int b){
+		return a+b;
+	}
+}
+
+class gitMain{
+
+	public static void main(String[] args){
+		testGit tg=new testGit();
+		System.out.println(tg.plus);
+	}
+}
